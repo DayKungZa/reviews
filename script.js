57ed6e9f39613dabf03f2e11c626bdad916e7d00
DayKungZa <tawanday2006@gmail.com>
2024-12-14 19:20:36 +0700

    using json header

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const repoLink = "https://raw.githubusercontent.com/DayKungZa/reviews/main/";
 const textID = "content";
-const headerFooterText = `
-<div class="button-row" id="games"> |
-    <span onclick="selectTopic(this, 'games/kirbyFL')">Kirby Forgotten Land</span> |
-    <span onclick="selectTopic(this, 'games/Xenoblade2TGC')">Xenoblade 2 Torna</span> |
-    <span onclick="selectTopic(this, 'games/Xenoblade3FR')">Xenoblade 3 Future Redeemed</span> |
-    <span onclick="selectTopic(this, 'games/mariowonder')">Super Mario Wonder</span> |
-    <span onclick="selectTopic(this, 'games/marioluigi2')">Mario and Luigi 2</span> |
-    <span onclick="selectTopic(this, 'games/marioluigi3')">Mario and Luigi 3</span> |
-    <span onclick="selectTopic(this, 'games/ittakestwo')">It Takes Two</span> |
-    <span onclick="selectTopic(this, 'games/celeste')">Celeste</span> |
-    <span onclick="selectTopic(this, 'games/metroidzm')">Metroid Zero Mission</span> |
-    <span onclick="selectTopic(this, 'games/metroidp1')">Metroid Prime Remastered</span> |
-
-</div>
-<div class="button-row" id="movies"> |
-    <span onclick="selectTopic(this, 'movies/bluegiant')">Blue Giant</span> |
-    <span onclick="selectTopic(this, 'movies/frieren')">Frieren</span>
-    <span onclick="selectTopic(this, 'movies/doraemon2024')">Doraemon 2024</span> |
-    <span onclick="selectTopic(this, 'movies/panda4')">Kung Fu Panda 4</span>
-    <span onclick="selectTopic(this, 'movies/lahnma')">หลานม่า</span> |
-    <span onclick="selectTopic(this, 'movies/whiplash')">Whiplash</span> |
-</div>
-`;
 let clicked = false;
+const headerFooterText = loadHeaderFooter();
+addHeader();
+
+async function loadHeaderFooter() {
+  try {
+    const response = await fetch('topics.json');
+    if (!response.ok) throw new Error(`Failed to fetch topics.json: ${response.statusText}`);
+    
+    const data = await response.json();
+
+    let headerFooterHTML = '';
+
+    // Add Games Section
+    headerFooterHTML += `<div class="button-row" id="games"> |`;
+    data.games.forEach(game => {
+      headerFooterHTML += `<span onclick="selectTopic(this, '${game.link}')">${game.name}</span> |`;
+    });
+    headerFooterHTML += `</div>`;
+
+    // Add Movies Section
+    headerFooterHTML += `<div class="button-row" id="movies"> |`;
+    data.movies.forEach(movie => {
+      headerFooterHTML += `<span onclick="selectTopic(this, '${movie.link}')">${movie.name}</span> |`;
+    });
+    headerFooterHTML += `</div>`;
+    return headerFooterHTML;
+  } catch (error) {
+      console.error('Error loading headerFooter:', error);
+      return `<div>error fetching topics.json</div>`;
+  }
+}
+
 
 async function loadMarkdown(reviewLink) {
   try {
@@ -61,7 +70,7 @@ function selectTopic(span, reviewLink) {
   document.querySelectorAll('.button-row span').forEach(el => el.classList.remove('active'));
   span.classList.add('active');
   loadMarkdown(reviewLink);
-  const targetElement = document.getElementById("content");
+  const targetElement = document.getElementById("text-container");
   if (targetElement) {
     targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
@@ -75,4 +84,3 @@ function addFooter() {
   const footer = document.getElementsByClassName("headerFooter")[1];
   footer.innerHTML = headerFooterText;
 }
-addHeader();
