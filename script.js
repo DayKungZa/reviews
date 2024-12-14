e4e32bcf3bb74a71aafa6079b2b84967cb61e2f4
DayKungZa <tawanday2006@gmail.com>
2024-12-14 13:41:05 +0700

    review web v1

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ console.log(typeof marked);
 
 async function loadMarkdown(reviewLink) {
   try {
-      const response = await fetch(`${repoLink}${reviewLink}`);
+      const response = await fetch(`${repoLink}${reviewLink}.md`);
       if (!response.ok) throw new Error(`Failed to fetch README.md: ${response.statusText}`);
       
       const markdown = await response.text();
       const readmeText = marked.parse ? marked.parse(markdown) : marked(markdown);
       
-      document.getElementById(textID).innerHTML = readmeText;
+      document.getElementById(textID).innerHTML = `
+        <img src="${reviewLink}.png" class="slide-image"></img>
+        ${readmeText}
+      `;
   } catch (error) {
       console.error(error);
       document.getElementById(textID).innerText = "Failed to load content.";
